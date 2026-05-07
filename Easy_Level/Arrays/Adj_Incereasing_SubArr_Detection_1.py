de837b5a30b9145dd84851b1c5fe40cac1547d68
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-05-07 22:59:43 +0530

    Implement function to detect increasing subarrays

---
+++ Easy_Level/Arrays/Adj_Incereasing_SubArr_Detection_1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        def is_strictly_increasing(start: int) -> bool:
+            for i in range(start, start + k - 1):
+                if nums[i] >= nums[i + 1]:
+                    return False
+            return True
+        for i in range(n - 2 * k + 1):
+            if is_strictly_increasing(i) and is_strictly_increasing(i + k):
+                return True
+        return False
