6c06db0c838648c68d6341a60781ab42949ec95c
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-05-07 22:29:43 +0530

    Implement plusOne function to increment digit array

---
+++ Easy_Level/Arrays/Plus_One.py
@@ -0,0 +1,10 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        # If we exited the loop, all digits were 9, e.g., [9,9,9]
+        return [1] + [0] * n
