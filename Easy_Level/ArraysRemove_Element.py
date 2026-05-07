e609542991f6445d40899751837a762aa6a317f1
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-05-07 22:19:35 +0530

    Delete Easy_Level/ArraysRemove_Element.py

--- Easy_Level/ArraysRemove_Element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                # Move valid element to the front
-                nums[k] = nums[i]
-                k += 1
-        return k
