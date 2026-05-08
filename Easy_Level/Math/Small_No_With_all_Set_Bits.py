c83c2e615666b1b84f0de30af517015264288c18
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-05-08 11:49:22 +0530

    solution to find smallest number with all set bits

---
+++ Easy_Level/Math/Small_No_With_all_Set_Bits.py
@@ -0,0 +1,6 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        x = 1
+        while x < n:
+            x = (x << 1) | 1  # shift left and set next bit to 1
+        return x
