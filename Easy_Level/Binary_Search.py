8f943aedd068460f78ba0071cf4c47650785aadb
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-06-07 17:09:23 +0530

    Implement binary search algorithm

---
+++ Easy_Level/Binary_Search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        s = 0
+        e = len(nums) - 1
+        while s <= e:
+            mid = (s + e) // 2
+            if target == nums[mid]:
+                return mid  # return the index of the target
+            elif target < nums[mid]:
+                e = mid - 1
+            else:
+                s = mid + 1
+        return -1  # return -1 if target is not found
