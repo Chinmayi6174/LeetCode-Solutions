abc37221798a7425f42dbcf243f6bffa49332671
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-06-07 17:22:17 +0530

    Implement minDepth function for binary tree

---
+++ Easy_Level/Min_Depth_of_BT.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = deque([(root, 1)])  # (node, current_depth)
+        while queue:
+            node, depth = queue.popleft()
+            # If this is a leaf node, we've found the minimum depth
+            if not node.left and not node.right:
+                return depth
+            if node.left:
+                queue.append((node.left, depth + 1))
+            if node.right:
+                queue.append((node.right, depth + 1))
