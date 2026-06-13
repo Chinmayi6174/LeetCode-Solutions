82693171f11d522fcabf7461fc3abd7d745dccee
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-06-13 13:16:10 +0530

    Implement alternative methods for counting subarrays
    Added an alternative approach using prefix sum to count subarrays.

--- Medium_Level/Count_No.of_Subarrays.py
+++ Medium_Level/Count_No.of_Subarrays.py
@@ -14,3 +14,20 @@ class Solution:
                 count += right - left + 1
             return count
         return atMost(k) - atMost(k - 1)
+
+#another approach
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        prefix_sum=0
+        count= {0:1}
+        ans=0
+        for num in nums:
+            if num%2==1:
+                prefix_sum+=1
+            if prefix_sum -k in count:
+                ans+= count[prefix_sum-k]
+            if prefix_sum in count:
+                count[prefix_sum]+=1
+            else:
+                count[prefix_sum]=1
+        return ans
