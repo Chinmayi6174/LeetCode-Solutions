f9e35b772fda578443659fc3ac26bd3ca71c86e2
Chinmayi6174 <kandikatla.chinmayi@gmail.com>
2026-06-14 10:26:30 +0530

    Implement alternative method for longest substring length
    Added another approach to find the length of the longest substring without repeating characters.

--- Medium_Level/Longest_Substring_Without_Repeating_Char.py
+++ Medium_Level/Longest_Substring_Without_Repeating_Char.py
@@ -9,3 +9,28 @@ class Solution:
             char_index[s[right]] = right
             max_len = max(max_len, right - left + 1)
         return max_len
+
+
+#another approach
+def lenoflongsubstr(self, s):
+    n=len(s)
+    left=0
+    ml=0
+    chset=set()
+    for r in range(n):
+        if s[r] not in chset:
+            chset.add(s[r])
+            ml=max(ml, r-left+1)
+        else:
+            ml=0
+            left=0
+            z=0
+            for r in range(len(nums)):
+                if nums[r]==0:
+                    z+=1
+                while z>k:
+                    if nums[left]==0:
+                        z-=1
+                    left+=1
+                ml=max(ml, r-left+1)
+            return ml
